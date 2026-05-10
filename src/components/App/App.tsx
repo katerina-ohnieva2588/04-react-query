8345097ce18ae568349817fe518a590a1a0c2fe5
Katerina Ohnieva <brilliantskate2588@gmail.com>
2026-05-10 22:10:33 +0300

    fix: move App component to components folder

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { fetchMovies } from "./services/movieService";
-import type { Movie } from "./types/movie";
+import { fetchMovies } from "../../services/movieService";
+import type { Movie } from "../../types/movie";
 
-import SearchBar from "./components/SearchBar/SearchBar";
-import MovieGrid from "./components/MovieGrid/MovieGrid";
-import Loader from "./components/Loader/Loader";
-import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
-import MovieModal from "./components/MovieModal/MovieModal";
+import SearchBar from "../SearchBar/SearchBar";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 import { toast, Toaster } from "react-hot-toast";
 
 export default function App() {
